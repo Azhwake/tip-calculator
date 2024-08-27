931aa52ab2afc3b39b8479e1ed8f8acf1db7d6da
Azhwake <azhwaq2002@gmail.com>
2024-08-27 14:48:18 +0300

    DONE

--- challenge.js
+++ challenge.js
@@ -1,2 +1,16 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let bill = 275; 
+
+let tip;
+if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+} else {
+    tip = bill * 0.20;
+}
+
+let total = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${total}`);
+
